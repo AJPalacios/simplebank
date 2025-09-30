d89468f464f7de48d64a76f77a4cf69bb9af8b9a
AJPalacios <ajpalacios.sist@gmail.com>
2025-09-30 11:35:14 -0600

    Add configuration loader using viper; update main and tests to use config file

--- main.go
+++ main.go
@@ -6,24 +6,24 @@ import (
 
 	"github.com/devspace/simplebank/api"
 	db "github.com/devspace/simplebank/db/sqlc"
+	"github.com/devspace/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://myuser:mysecretpassword@localhost:5432/simple_bank?sslmode=disable"
-	address  = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
-	err = server.Start(address)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
